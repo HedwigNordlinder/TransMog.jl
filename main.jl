aac9eec029c8f62aaede145a9205e8ec3d393d02
hedwignordlinder <hedwignordlinder@gmail.com>
2026-04-21 16:22:41 +0200

    Loss implementation

--- main.jl
+++ main.jl
@@ -18,6 +18,7 @@ function TransMogModel(; embeddim = 128, spacedim = 2, layers = 3, n_gaussians =
     decode_log_variances = Chain(Dense(embeddim => embeddim, swish), Dense(embeddim => spacedim * n_gaussians))
     decode_log_weights = Chain(Dense(embeddim => embeddim, swish),Dense(embeddim => n_gaussians))
     layers = (; embed_time_t, embed_time_s, embed_state, projection_layer, ffs, decode_means, decode_log_variances, decode_log_weights)
+    TransMogModel(layers)
 end
 
 function (f::TransMogModel)(s, t, Xs)
@@ -32,6 +33,20 @@ function (f::TransMogModel)(s, t, Xs)
     return l.decode_means(x), l.decode_log_variances(x), l.decode_log_weights(x)
 end
 
-function negative_log_likelihood(means,log_variances, log_weights, Xt)
-    
+function negative_log_likelihood(means, log_variances, log_weights, Xt)
+    x = tensor(Xt)
+    D = size(x, 1)
+    K = size(log_weights, 1)
+    μ  = reshape(means,          D, K, :)
+    lv = reshape(log_variances,  D, K, :)
+    xr = reshape(x,              D, 1, :)
+    diff = xr .- μ
+    log2π = Float32(log(2π))
+    log_gauss = dropdims(
+        sum(-0.5f0 .* (log2π .+ lv .+ diff .^ 2 .* exp.(-lv)); dims = 1);
+        dims = 1,
+    )
+    logp = Flux.logsumexp(log_weights .+ log_gauss; dims = 1) .-
+           Flux.logsumexp(log_weights;              dims = 1)
+    return -sum(logp) / length(logp)
 end
